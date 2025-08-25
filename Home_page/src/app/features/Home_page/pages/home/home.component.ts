d60a2cada15f32fed9007c583ca1f8b5d7b62200
Ahmed Mohsen <ahmedazab05@gmail.com>
2025-08-25 13:54:34 +0300

    floating chatbot button

--- Home_page/src/app/features/Home_page/pages/home/home.component.ts
+++ Home_page/src/app/features/Home_page/pages/home/home.component.ts
@@ -3,9 +3,11 @@ import { Hero } from '../../components/hero/hero.component';
 import { CategoryList } from '../../components/category-list/category-list.component';
 import { ProductGrid } from '../../components/product-grid/product-grid.component';
 import {SearchBar} from '../../components/search-bar/search-bar.component';
+import {ChatbotButton} from '../../../../shared/components/chatbot-button/chatbot-button.component';
+
 @Component({
   selector: 'app-home',
-  imports: [Hero, CategoryList, ProductGrid, SearchBar],
+  imports: [Hero, CategoryList, ProductGrid, SearchBar, ChatbotButton],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ Home_page/src/app/shared/components/chatbot-button/chatbot-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbot-button',
+  imports: [],
+  templateUrl: './chatbot-button.component.html',
+  styleUrl: './chatbot-button.component.css'
+})
+export class ChatbotButton {
+
+}
